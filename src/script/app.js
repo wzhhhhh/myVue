ead2155675f846cda0e5a5afee98187632658a6c
wzhhhhh <13161987838@163.com>
2017-05-04 11:58:39 +0800

    ss

--- src/script/app.js
+++ src/script/app.js
@@ -11,10 +11,18 @@ let vm = new Vue({
 			this.lis = this.lis.concat(text);
 			// this.lis.push(text)
 			this.text = '';
-		}
+		},
 		// delete: function(index){
 		// 	this.lis = this.lis.splice(index, 1)
 		// }
+		getDate: function(){
+			return new Date().getTime()
+		}
+	},
+	computed: {
+		getDate1: function(){
+			return new Date().getTime()
+		}
 	}
 })
-
+window.vm = vm;
